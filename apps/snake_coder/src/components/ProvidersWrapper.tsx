a1add9a4000fd971d9bd46a1a74f2e0b5f50f198
Patryk Gajewski <patgaj15@gmail.com>
2025-12-01 23:35:31 +0100

    chore: add and integrate TanStack Query

--- apps/snake_coder/src/components/ProvidersWrapper.tsx
+++ apps/snake_coder/src/components/ProvidersWrapper.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SessionProvider } from "next-auth/react";
 import type { ReactNode } from "react";
 
@@ -7,6 +9,14 @@ type ProviderWrapperProps = {
   children: ReactNode;
 };
 
-export const ProviderWrapper: React.FC<ProviderWrapperProps> = ({ children}) => {
-  return <SessionProvider>{children}</SessionProvider>;
+export const ProviderWrapper: React.FC<ProviderWrapperProps> = ({ children }) => {
+  const queryClient = new QueryClient();
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </SessionProvider>
+  );
 };

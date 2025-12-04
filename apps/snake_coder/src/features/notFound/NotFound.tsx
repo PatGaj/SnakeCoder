37bd7c3835536ceacdb5744482d1ba7ddd9c6241
Patryk Gajewski <patgaj15@gmail.com>
2025-12-04 21:32:47 +0100

    feat: add simple not found graphic and display component

---
+++ apps/snake_coder/src/features/notFound/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <img src="/images/notFound.png" className="w-1/2" />
+    </div>
+  )
+}
+
+export default NotFound
